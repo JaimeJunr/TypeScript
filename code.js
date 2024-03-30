44ce1d71d414c1c551458c542e650bbc2a633fa7
Jaime Junr <jaimeodairbassojuniorjaime@gmail.com>
2024-03-29 23:16:50 -0300

    initial commit

---
+++ code.js
@@ -0,0 +1,7 @@
+function multi(x, y) {
+    return x * y;
+}
+function saudacao(name) {
+    return "Ola ".concat(name);
+}
+console.log(saudacao("Jaime"));
---
+++ code.ts
@@ -0,0 +1,11 @@
+function multi(x: number, y: number): number {
+    return x*y;
+}
+
+function saudacao(name:string):string{
+    return `Ola ${name}`;
+}
+
+console.log(saudacao("Jaime"))
+
+console.assert(true)
